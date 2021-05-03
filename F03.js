6ecd1078240969e42ea6e27df63dbffef9decb1c
petern1953 <petern1953@gmail.com>
2021-05-03 12:18:46 +0200

    3

--- F03.js
+++ F03.js
@@ -10,4 +10,10 @@ const converterBtwNumberSystems = (decimal) => ({
     hexa: `0x${(decimal >>> 0).toString(16)}`
 });
 
+const converterBtwNumberSystems2 = (decimal) => ({
+    binary: parseInt(decimal, 2),
+    octal: parseInt(decimal, 8),
+    hexa: parseInt(decimal, 16)
+});
 console.log(converterBtwNumberSystems(16));
+console.log(converterBtwNumberSystems2(16));
